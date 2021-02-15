ecbbac69d690357dde35e34d1db3e4dbef6a695b
saulsanto22 <saul.santoa@gmail.com>
2021-02-14 16:15:05 -0800

    melakukan update di config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "users";
+$db_host = "sql204.epizy.com";
+$db_user = "epiz_27930389";
+$db_pass = "VpPe8iy47fFUJ0b";
+$db_name = "epiz_27930389_users";
 
 try {    
     //create PDO connection 
